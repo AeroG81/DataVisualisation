06edf9e60b11e5a3da70c234eee91880e2ecbee5
wpan0017 <wpan0017@student.monash.edu>
2021-09-24 21:43:04 +0800

    updated url

--- docs/js/vega_lite_vis.js
+++ docs/js/vega_lite_vis.js
@@ -3,4 +3,4 @@ vegaEmbed("#F1map", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-console.log("Newer version 2");
+console.log("Newer version 3");
