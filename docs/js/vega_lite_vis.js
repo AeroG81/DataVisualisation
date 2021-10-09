a587ddfc66118b65e6588b60ac806a4643384f79
wpan0017 <wpan0017@student.monash.edu>
2021-10-09 21:37:12 +0800

    update new data

--- docs/js/vega_lite_vis.js
+++ docs/js/vega_lite_vis.js
@@ -3,4 +3,9 @@ vegaEmbed("#F1map", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-console.log("Newer version 3");
+var vg_2 = "js/f1_constructor.vg.json";
+vegaEmbed("#F1constructor", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+console.log("Newer version 4");
